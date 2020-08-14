410b39b849917c97f8cf4f9baa424dbc08816550
Danilo Vitoriano <vitoriano08@gmail.com>
2020-08-14 20:20:37 -0300

    add post route and use json middleware

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+let items = []
+
+app.use(express.json())
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.json(items[1])
+})
+
+app.post('/', (req, res) => {
+    items.push(req.body.name)
+    console.log(req.body)
+    res.send(`Criando dados de ${req.body.name}`)
 })
 
 app.listen(port, () => {
